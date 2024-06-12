eff861cefea674936c318b2ec139f43904478832
AJ-AYUSHMAN <sonurajsheohar@gmail.com>
2024-06-12 16:14:02 +0530

    Adding files

---
+++ Project_1/Assets/Scripts/vs setup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vssetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log("hello this is Ayushman");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
